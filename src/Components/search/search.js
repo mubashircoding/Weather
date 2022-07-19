837e3a8ec0d51be5e02b01c5f2be824baf7c65a3
mubashircoding <mubashirkaiser2009@gmail.com>
2022-07-18 21:25:48 -0400

    c

--- src/Components/search/search.js
+++ src/Components/search/search.js
@@ -5,9 +5,10 @@ const Search = ({ OnSearchChange }) => {
   const [search, setSearch] = useState(null);
   const loadOptions = (inputValue) => {
     return fetch(`${GEO_API_URL}/cities?minPopulation=1000000000&namePrefix=${inputValue}`, geoApiOptions)
-      .then(response => response.json())
-      .then(response => console.log(response))
-      .catch(err => console.error(err));
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
+
   }
 
   const handleOnChange = (searchData) => {
--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 export const geoApiOptions = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
+		'X-RapidAPI-Key': 'a314d84020mshba5305db5f522d1p13bee9jsn8e0c416d0e74',
 		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
 	}
 };
