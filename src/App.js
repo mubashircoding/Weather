a52a7cc154aee2ffd3d6eccfbedd4b77beedc9ff
mubashircoding <mubashirkaiser2009@gmail.com>
2022-07-20 13:35:11 -0400

    gg

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import './App.css';
 
 import CurrentWeather from './Components/current-weather/current-weather';
 import Search from './Components/search/search';
+import {WEATHER_API_URL, WEATHER_API_KEY} from './api'
 function App() {
 
 
   const handleOnSearchChange = (searchData) =>{
 
-    console.log(searchData);
+    const{lat,lon}=searchData.value.split(" ");
+
+    const currentWeatherfetch=fetch(`${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`)
+
+    api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 
   }
 
