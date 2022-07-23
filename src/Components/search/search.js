1291742e2b19b0990279a98e6d000264905e9b9f
mubashircoding <mubashirkaiser2009@gmail.com>
2022-07-22 20:25:49 -0400

    g

--- src/Components/search/search.js
+++ src/Components/search/search.js
@@ -1,30 +1,32 @@
+import React, { useState } from "react";
 import { AsyncPaginate } from "react-select-async-paginate";
-import React, { useState } from 'react'
-import { GEO_API_URL, geoApiOptions } from '../../api';
-const Search = ({ OnSearchChange }) => {
+import { geoApiOptions, GEO_API_URL } from "../../api";
+
+const Search = ({ onSearchChange }) => {
   const [search, setSearch] = useState(null);
+
   const loadOptions = (inputValue) => {
-    return fetch(`${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`, geoApiOptions)
-      .then(response => response.json())
-      .then(response => {
+    return fetch(
+      `${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`,
+      geoApiOptions
+    )
+      .then((response) => response.json())
+      .then((response) => {
         return {
           options: response.data.map((city) => {
             return {
-              value: `${city.latitude} ${city.longitude} `,
+              value: `${city.latitude} ${city.longitude}`,
               label: `${city.name}, ${city.countryCode}`,
-            }
-          })
-        }
-      })
-      .catch(err => console.error(err));
-
-  }
+            };
+          }),
+        };
+      });
+  };
 
   const handleOnChange = (searchData) => {
     setSearch(searchData);
-    OnSearchChange(searchData);
+    onSearchChange(searchData);
   };
-
 
   return (
     <AsyncPaginate
@@ -35,5 +37,6 @@ const Search = ({ OnSearchChange }) => {
       loadOptions={loadOptions}
     />
   );
-}
+};
+
 export default Search;
--- src/api.js
+++ src/api.js
@@ -1,11 +1,11 @@
 export const geoApiOptions = {
-	method: 'GET',
+	method: "GET",
 	headers: {
-		'X-RapidAPI-Key': 'a314d84020mshba5305db5f522d1p13bee9jsn8e0c416d0e74',
-		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
-	}
-};
-export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
-
-export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
-export const WEATHER_API_KEY = "eac7c86badc90353b31f4afc57648493";
+	  "X-RapidAPI-Key": "",// enter your rapid api key here
+	  "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+	},
+  };
+  export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+  
+  export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+  export const WEATHER_API_KEY = ""; // enter your key from openweather API
