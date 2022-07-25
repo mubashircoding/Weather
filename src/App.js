623da71955e90257cd234b6cbbdb72c9059a5c1e
mubashircoding <mubashirkaiser2009@gmail.com>
2022-07-25 15:45:43 -0400

    ghhg

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
 
     const currentWeatherFetch = fetch(`${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`)
     const forecastFetch = fetch(`${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`)
-
+    
     Promise.all([currentWeatherFetch, forecastFetch])
       .then(async (response) => {
         const weatherResponse = await response[0].json();
