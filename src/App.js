0dec8edfc35c8d26ed8c5bf224cbcf492052be32
mubashircoding <mubashirkaiser2009@gmail.com>
2022-07-26 15:33:08 -0400

    bb

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
 
   const handleOnSearchChange = (searchData) => {
 
-    const { lat, lon } = searchData.value.split(" ");
+    const [ lat, lon ] = searchData.value.split(" ");
 
     const currentWeatherFetch = fetch(`${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`)
     const forecastFetch = fetch(`${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`)
@@ -22,7 +22,7 @@ function App() {
       .then(async (response) => {
         const weatherResponse = await response[0].json();
         const forecastResponse = await response[1].json();
-        setCurrentWeather({ city: searchData.label, weatherResponse });
+        setCurrentWeather({ city: searchData.label, ...weatherResponse });
         setForecast({ city: searchData.label, ...forecastResponse });
       })
       .catch((err) => console.log(err));
@@ -33,7 +33,7 @@ function App() {
   return (
     <div className="container">
       <Search onSearchChange={handleOnSearchChange} />
-      {currentWeather && <CurrentWeather data={currentWeather} />}
+       {currentWeather&&<CurrentWeather data={currentWeather} />}
     </div>
 
   );
